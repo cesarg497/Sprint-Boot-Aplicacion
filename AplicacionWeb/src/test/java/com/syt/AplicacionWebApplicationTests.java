67e08fa7e9f097d11ede75dca0de20fb1d8ccfd2
Cesar Grance <grancec@gmail.com>
2021-05-02 09:38:46 -0400

    Commit Inicial con Eclipse

---
+++ AplicacionWeb/src/test/java/com/syt/AplicacionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
